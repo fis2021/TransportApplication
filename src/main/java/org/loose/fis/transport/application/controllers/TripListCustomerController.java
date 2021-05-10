303b552a12a2551d9fa51a5969ee01ef81a54bf4
Sabina Cenda <sabina.cenda17@gmail.com>
2021-05-10 22:03:21 +0300

    PROIEC-7 Added logout button functionality

--- src/main/java/org/loose/fis/transport/application/controllers/TripListCustomerController.java
+++ src/main/java/org/loose/fis/transport/application/controllers/TripListCustomerController.java
@@ -3,14 +3,20 @@ package org.loose.fis.transport.application.controllers;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.loose.fis.transport.application.model.Trip;
 import org.loose.fis.transport.application.services.TripService;
+
+import java.io.IOException;
 
 public class TripListCustomerController {
 
@@ -86,7 +92,14 @@ public class TripListCustomerController {
 
     @FXML
     void handleLogoutButton(ActionEvent event) {
-
+        try{
+            Stage stage = (Stage) TEXT.getScene().getWindow();
+            Parent viewStudentsRoot = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+            Scene scene = new Scene(viewStudentsRoot, 600, 400);
+            stage.setScene(scene);
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
