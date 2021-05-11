00b60ccd50e2958764b88ce2a7a42429bc9b5f15
Emanuel Bancila <bancila.emanuel953@gmail.com>
2021-05-11 22:30:14 +0300

    PROIEC-9 Added back button functionality

--- src/main/java/org/loose/fis/transport/application/controllers/PastDeliveriesController.java
+++ src/main/java/org/loose/fis/transport/application/controllers/PastDeliveriesController.java
@@ -3,11 +3,15 @@ package org.loose.fis.transport.application.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.loose.fis.transport.application.model.DeliveryRequest;
 import org.loose.fis.transport.application.model.TripRequest;
 import org.loose.fis.transport.application.model.TripRequest2;
@@ -15,6 +19,7 @@ import org.loose.fis.transport.application.services.DeliveryRequestService;
 import org.loose.fis.transport.application.services.TripRequestService;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class PastDeliveriesController {
     @FXML
@@ -48,12 +53,26 @@ public class PastDeliveriesController {
         colDeliveryAdress.setCellValueFactory(new PropertyValueFactory<DeliveryRequest,String>("deliveryAddress"));
         colAditionalInformation.setCellValueFactory(new PropertyValueFactory<DeliveryRequest,String>("additionalInformation"));
         colStatus.setCellValueFactory(new PropertyValueFactory<DeliveryRequest,String>("status"));
-
-        table.setItems(DeliveryRequestService.Lista());
+        ObservableList<DeliveryRequest>l=DeliveryRequestService.Lista();
+        ObservableList<DeliveryRequest>list= FXCollections.observableArrayList();
+        for (DeliveryRequest k:
+                l) {
+            if(k.getCustomerName().equals(LoginController.customerName)){
+                list.add(k);
+            }}
+        table.setItems(list);
     }
 
 
 
     public void handleBackButton(javafx.event.ActionEvent actionEvent) {
+        try {
+            Stage stage = (Stage) TEXT.getScene().getWindow();
+            Parent viewStudentsRoot = FXMLLoader.load(getClass().getClassLoader().getResource("tripListCustomer.fxml"));
+            Scene scene = new Scene(viewStudentsRoot, 900, 700);
+            stage.setScene(scene);
+        } catch (IOException p) {
+            p.printStackTrace();
+        }
     }
 }
