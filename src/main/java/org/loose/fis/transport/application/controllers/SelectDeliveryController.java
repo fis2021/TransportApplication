8f4ae38f3a763995bee12662088f01a2625af5fc
Sabina Cenda <sabina.cenda17@gmail.com>
2021-05-11 14:01:40 +0300

    PROIEC-8 Created fxml file for booking deliveries GUI

---
+++ src/main/java/org/loose/fis/transport/application/controllers/SelectDeliveryController.java
@@ -0,0 +1,38 @@
+package org.loose.fis.transport.application.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SelectDeliveryController {
+
+    @FXML
+    private TextField pickupAddress;
+
+    @FXML
+    private TextField vehicleType;
+
+    @FXML
+    private TextField deliveryAddress;
+
+    @FXML
+    private TextField additionalInformation;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button requestDeliveryButton;
+
+    @FXML
+    void handleBackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRequestDeliveryButton(ActionEvent event) {
+
+    }
+
+}
