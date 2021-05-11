31e5671d3e60d6c3cb3f81fcd2d4bd27f5d7730f
Emanuel Bancila <bancila.emanuel953@gmail.com>
2021-05-11 21:59:18 +0300

    PROIEC-9 Created new class TripRequest2 for displaying past trips and created controller for past trips

---
+++ src/main/java/org/loose/fis/transport/application/controllers/PastBookingsController.java
@@ -0,0 +1,86 @@
+package org.loose.fis.transport.application.controllers;
+
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import org.jetbrains.annotations.NotNull;
+import org.loose.fis.transport.application.model.Trip;
+import org.loose.fis.transport.application.model.TripRequest;
+import org.loose.fis.transport.application.model.TripRequest2;
+import org.loose.fis.transport.application.services.TripRequestService;
+import org.loose.fis.transport.application.services.TripService;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+
+public class PastBookingsController {
+    @FXML
+    private TableView<TripRequest2> table;
+
+    @FXML
+    private TableColumn<TripRequest2, String> colVehicleType;
+
+    @FXML
+    private TableColumn<TripRequest2, Integer> colSpace;
+
+    @FXML
+    private TableColumn<TripRequest2, String> colDate;
+
+    @FXML
+    private TableColumn<TripRequest2, String> colTime;
+
+    @FXML
+    private TableColumn<TripRequest2, Integer> colPrice;
+
+    @FXML
+    private TableColumn<TripRequest2, String> colRoute;
+
+    @FXML
+    private TableColumn<TripRequest2, String> colStatus;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Text TEXT;
+    @FXML
+    public void initialize() {
+
+        colVehicleType.setCellValueFactory(new PropertyValueFactory<TripRequest2,String>("vehicleType"));
+        colSpace.setCellValueFactory(new PropertyValueFactory<TripRequest2,Integer>("space"));
+        colDate.setCellValueFactory(new PropertyValueFactory<TripRequest2,String>("date"));
+        colTime.setCellValueFactory(new PropertyValueFactory<TripRequest2,String>("time"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<TripRequest2,Integer>("price"));
+        colRoute.setCellValueFactory(new PropertyValueFactory<TripRequest2,String>("route"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<TripRequest2,String>("approved"));
+
+        ObservableList<TripRequest>l=TripRequestService.Lista();
+        ObservableList<TripRequest2>list= FXCollections.observableArrayList();
+        for (TripRequest k:
+             l) {
+            String a="Pending";
+            if(k.getApproved()==0)
+                a="Denied";
+            if(k.getApproved()==1)
+                a="Approved";
+            list.add(new TripRequest2(k.getTrip().getVehicleType(),k.getTrip().getSpace(),k.getTrip().getDate(),k.getTrip().getTime(),k.getTrip().getPrice(),k.getTrip().getRoute(),a));
+        }
+        table.setItems(list);
+    }
+
+    @FXML
+    void handleBackButton(ActionEvent event) {
+
+    }
+}
