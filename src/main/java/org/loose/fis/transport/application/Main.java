5f44125af3727d844615294311531be58e1005f2
Emanuel Bancila <bancila.emanuel953@gmail.com>
2021-05-12 20:01:32 +0300

    PROIEC-45 Modified files to be able to create and run tests

--- src/main/java/org/loose/fis/transport/application/Main.java
+++ src/main/java/org/loose/fis/transport/application/Main.java
@@ -15,7 +15,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        initDirectory();
         UserService.initDatabase();
         VehicleService.initDatabase();
         TripService.initDatabase();
@@ -28,11 +27,6 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private void initDirectory() {
-        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
-        if (!Files.exists(applicationHomePath))
-            applicationHomePath.toFile().mkdirs();
-    }
 
     public static void main(String[] args) {
         launch(args);
